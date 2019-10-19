59c906c946d1944f065c76e6998caad4cbfff203
downlz <shahnawaz_haq@outlook.com>
2019-10-19 14:44:35 +0530

    publishing file

---
+++ api/core/utils.py
@@ -0,0 +1,23 @@
+from core.logging import logger
+from gmail import GMail, Message
+from config._passwords import EMAIL_PASSWORD, EMAIL_ADDRESS
+
+
+# Set up emailer
+gmail = GMail(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+def email(email, subject, body, html=None):
+    """ Sends email """
+    logger.info(
+        f'Sending email to {email}... '
+        f'Subject: {subject}  '
+        f'Body: {body}'
+        )
+    msg = Message(
+        subject,
+        to='%s <%s>' % (email, email),
+        text=body,
+        html=html
+        )
+
+    gmail.send(msg)
